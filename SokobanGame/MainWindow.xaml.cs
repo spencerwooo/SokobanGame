ea6c4312d6ff7d84c63456d07413403c9304ee00
spencerwooo <spencerwoo98@foxmail.com>
2019-05-29 16:27:45 +0800

    Add project files.

---
+++ SokobanGame/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SokobanGame
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void StartGame(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DragWindow(object sender, MouseButtonEventArgs e)
+    {
+      if (e.ChangedButton == MouseButton.Left)
+      {
+        DragMove();
+      }
+    }
+
+    private void CloseWindow(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
+  }
+}
